eadee3ba80e9d19f36e11eeac7c1bf5c3e54c9cf
smiling011 <victoriasaleck@gmail.com>
2025-09-08 23:30:16 -0500

    Actividad de fisica

---
+++ Actividad de Fisica/index.py
@@ -0,0 +1,9 @@
+# 1. La NASA quiere calcular la cantidad de energía necesaria para enviar
+# Se pide:
+# 1. Expresar los valores en notación científica.
+# 2. Calcular la energía cinética de la nave.
+# 3. Mostrar el resultado en notación científica.
+
+# RECUERDA QUE EL VALOR DE LA MASA DEBE SER DINAMICO, Y EL
+# PROGRMA DEBERÁ PEDIR DICHO DATO.
+
